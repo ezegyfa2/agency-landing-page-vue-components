6589aa9da324b7b96265f1cdb3cf2dc6e4d31024
Darvas Istvan <darvasvok@gmail.com>
2023-01-02 21:38:59 +0200

    v1.0.4

--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ export default {
 		require('description-vue-components').default.install(Vue);
 		require('helper-vue-components').default.install(Vue);
 		
-		Vue.component('agency-navigation-bar', require('./NavigationBar/NavigationBar/Component.vue').default);
-		Vue.component('agency-navigation-link', require('./NavigationBar/NavigationLink/Component.vue').default);
-		Vue.component('agency-header', require('./Header/Component.vue').default);
-		Vue.component('agency-services', require('./Services/Services/Component.vue').default);
-		Vue.component('agency-about', require('./About/Component.vue').default);
-		Vue.component('agency-timeline', require('./Timeline/Timeline/Component.vue').default);
-		Vue.component('agency-timeline-item', require('./Timeline/TimelineItem/Component.vue').default);
-		Vue.component('agency-team', require('./Team/Team/Component.vue').default);
-		Vue.component('agency-team-member', require('./Team/Member/Component.vue').default);
-		Vue.component('agency-clients', require('./Clients/Component.vue').default);
-		Vue.component('agency-contact-form', require('./ContactForm/Component.vue').default);
-		Vue.component('agency-footer', require('./Footer/Component.vue').default);
+		registerVueComponent('agency-navigation-bar', require('./NavigationBar/NavigationBar/Component.vue').default, Vue);
+		registerVueComponent('agency-navigation-link', require('./NavigationBar/NavigationLink/Component.vue').default, Vue);
+		registerVueComponent('agency-header', require('./Header/Component.vue').default, Vue);
+		registerVueComponent('agency-services', require('./Services/Services/Component.vue').default, Vue);
+		registerVueComponent('agency-about', require('./About/Component.vue').default, Vue);
+		registerVueComponent('agency-timeline', require('./Timeline/Timeline/Component.vue').default, Vue);
+		registerVueComponent('agency-timeline-item', require('./Timeline/TimelineItem/Component.vue').default, Vue);
+		registerVueComponent('agency-team', require('./Team/Team/Component.vue').default, Vue);
+		registerVueComponent('agency-team-member', require('./Team/Member/Component.vue').default, Vue);
+		registerVueComponent('agency-clients', require('./Clients/Component.vue').default, Vue);
+		registerVueComponent('agency-contact-form', require('./ContactForm/Component.vue').default, Vue);
+		registerVueComponent('agency-footer', require('./Footer/Footer/Component.vue').default, Vue);
+		registerVueComponent('agency-footer-link', require('./Footer/Link/Component.vue').default, Vue);
 
-		Vue.component('agency-landing-page', require('./LandingPage/Component.vue').default);
+		registerVueComponent('agency-landing-page', require('./LandingPage/Component.vue').default, Vue);
 
-		Vue.component('agency-large-text-content', require('./LargeTextContent/Component.vue').default);
-		Vue.component('agency-medium-text-content', require('./MediumTextContent/Component.vue').default);
-		Vue.component('agency-small-text-content', require('./SmallTextContent/Component.vue').default);
-		Vue.component('agency-text-content-with-icon', require('./Services/TextContentWithIcon/Component.vue').default);
+		registerVueComponent('agency-large-text-content', require('./LargeTextContent/Component.vue').default, Vue);
+		registerVueComponent('agency-medium-text-content', require('./MediumTextContent/Component.vue').default, Vue);
+		registerVueComponent('agency-small-text-content', require('./SmallTextContent/Component.vue').default, Vue);
+		registerVueComponent('agency-text-content-with-icon', require('./Services/TextContentWithIcon/Component.vue').default, Vue);
 	}
 };
